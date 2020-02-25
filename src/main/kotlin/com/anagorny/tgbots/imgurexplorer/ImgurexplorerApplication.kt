f44375748bcf746970eca8f4e271b38d5058e9f0
nagorny <mail@anagorny.com>
2020-02-25 08:23:46 +0300

    Init app

---
+++ src/main/kotlin/com/anagorny/tgbots/imgurexplorer/ImgurexplorerApplication.kt
@@ -0,0 +1,11 @@
+package com.anagorny.tgbots.imgurexplorer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ImgurexplorerApplication
+
+fun main(args: Array<String>) {
+	runApplication<ImgurexplorerApplication>(*args)
+}
---
+++ src/test/kotlin/com/anagorny/tgbots/imgurexplorer/ImgurexplorerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.anagorny.tgbots.imgurexplorer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ImgurexplorerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
